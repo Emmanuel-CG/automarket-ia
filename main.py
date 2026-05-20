7115ae31c58df6f21f92100c080400144e9250f9
Emmanuel-CG <22690367@tecvalles.mx>
2026-05-20 01:17:34 -0600

    actualizacion

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 import pandas as pd
 import joblib
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # cargar modelo
 model = joblib.load("modelo.pkl")
 
