1d7e54c24597a45548b8008946cebc22fcb90d73
Emmanuel-CG <22690367@tecvalles.mx>
2026-05-20 00:15:54 -0600

    actualizacion

---
+++ train.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import joblib
+
+# cargar dataset
+df = pd.read_csv("entrenamiento_autos_25k.csv")
+
+# columnas categóricas
+categorical = [
+    "Brand",
+    "Model",
+    "Fuel_Type",
+    "Transmission",
+    "City",
+    "Car_Type"
+]
+
+encoders = {}
+
+for col in categorical:
+    le = LabelEncoder()
+    df[col] = le.fit_transform(df[col])
+    encoders[col] = le
+
+# variables para entrenar
+X = df[
+    [
+        "Brand",
+        "Model",
+        "Year",
+        "Kilometer",
+        "Fuel_Type",
+        "Transmission",
+        "City",
+        "Car_Type"
+    ]
+]
+
+# precio objetivo
+y = df["Price_MXN"]
+
+# dividir dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# modelo IA
+model = RandomForestRegressor()
+
+# entrenar
+model.fit(X_train, y_train)
+
+# guardar modelo
+joblib.dump(model, "modelo.pkl")
+
+# guardar encoders
+joblib.dump(encoders, "encoders.pkl")
+
+print("MODELO ENTRENADO")
